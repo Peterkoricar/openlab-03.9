1a9dbc41c2ac56b294ece9ee86fe3cebd2f6251c
Peterkoricar <56106246+Peterkoricar@users.noreply.github.com>
2019-11-04 09:13:50 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openlab_03._9
+{
+    class Number
+    {
+        public bool IsPrimeNumber(int num)
+        {
+            if (num == 1)
+            {
+                return false;
+            }
+            for (int a = 2; a < num; a++)
+            {
+                if (num % a == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using openlab_03._9;
+
+namespace openlab_03._9
+{
+    class Program
+    {
+        public static void Main (string [] args)
+        {
+            var PNum = new Number();
+            for (int b = 1; b < 1000; b++)
+            {
+                Console.WriteLine($"{b};{PNum.IsPrimeNumber(b)}");
+            }
+        }
+    }
+}
